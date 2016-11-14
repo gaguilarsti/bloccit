1f23a0298337ae74ee609e10b1de89824aa7988f
Gama Aguilar <gama@9104studios.com>
2016-11-13 17:11:13 -0800

    checkpoint 17 seeding data completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'random_data'
+
+#Create Posts
+50.times do
+  #Adding a ! to the method instructs it raise an error if there's the problem with the data we're seeding.
+  Post.create!(
+  #Initially here, we referenced a class that doesn't exist.  This is wishful coding but ok.  :)
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+  )
+end
+
+posts = Post.all
+
+#Create comments
+#using an integer on .times will make sure this thing will run x times.
+100.times do
+  Comment.create!(
+    # call sample on the array returned by Post.all in order to pick a random post to associate with each comment.
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
