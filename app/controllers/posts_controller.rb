3cd2acb659a2924db2881238471c44ed09f6d55f
Gama Aguilar <gama@9104studios.com>
2016-11-15 23:59:27 -0800

    checkpoint 19 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,11 +6,36 @@ class PostsController < ApplicationController
   end
 
   def show
+    #we find the post that corresponds to the id in the params that was passed to show and assign it to @post.
+    @post = Post.find(params[:id])
   end
 
   def new
+    #create an instance variable, then assign it to an empty post returned by Post.new
+    @post = Post.new
   end
 
   def edit
   end
+
+  def create
+    #call Post.new to create a new instance of Post.
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    #if successfully save, prompt a message and redirect to the post that was created/saved.
+
+    if @post.save
+      #flash has provides a way to pass temporary values between actions.  Any value placed in flash will be available in the next action and then deleted.
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+    #if there was an error in saving, display error message and render the empty new template gain.
+      flash.now[:alert] = "There was an error saving the post.  Please try again."
+      render :new
+
+    end
+  end
+
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -19,21 +19,64 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
+  describe "GET #show" do
+    it "returns http success" do
+      get :show, {id: my_post.id}
+      expect(response).to have_http_status(:success)
+    end
 
+    it "renders the #show view" do
+      get :show, {id: my_post.id}
+      expect(response).to render_template :show
+    end
 
-  # describe "GET #show" do
-  #   it "returns http success" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  #
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+    it "assigns my_post to @post" do
+      get :show, {id: my_post.id}
+
+      expect(assigns(:post)).to eq(my_post)
+    end     
+  end
+
+  #when new is invoked a new but unsaved post object is created but not saved.
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    #expect PostsController#new to render the posts new view.
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    #expect @post instance variable to be initialized by PostsController#new.
+    #assigns gives us access to the @post variable, assigning it to :post.
+    it "instantiates @post" do
+      get :new
+      expect(assigns(:post)).not_to be_nil
+    end
+  end
+
+  describe "POST create" do
+    #when create is invoked, the newly created object is in the database.
+    #expect that PostsController#create is called with the parameters below and count of Post instances in the db increases by 1.
+    it "increases the number of Post by 1" do
+      expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
+    end
+
+    it "assigns the new post to @post" do
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      expect(assigns(:post)).to eq Post.last
+    end
+
+    #expect to be redirected to the newly created post.
+    it "redirects to the new post" do
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      expect(response).to redirect_to Post.last
+    end
+  end
+
   #
   # describe "GET #edit" do
   #   it "returns http success" do
