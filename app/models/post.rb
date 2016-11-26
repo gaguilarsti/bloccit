37c553a9c74a41ac5978cbd32f2ae21ae6a07c1c
Gama Aguilar <gama@9104studios.com>
2016-11-26 10:09:09 -0800

    assignment 27 completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,16 @@ class Post < ActiveRecord::Base
   #added this to show posts in descending order chronologically
   default_scope { order('created_at DESC') }
 
+  # ***assignment 27 additional scopes***
+
+  #ordered by title
+  scope :ordered_by_title, -> { order('title ASC')}
+
+  #ordered by reverse created at
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC')}
+
+
+
   #data validations
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
